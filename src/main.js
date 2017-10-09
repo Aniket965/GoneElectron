301602158b4ad096e959fcf0cb86347043722871
aniket sharma <aniket965.as@gmail.com>
2017-10-09 21:16:10 +0530

    theme one

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-const electron = require('electron');
+const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 app.on('ready', _ => {
-    mainWindow = new BrowserWindow({
-        width: 400,
-        height: 300
-    })
-    mainWindow.loadURL(`file://${__dirname}/index.html`)
+  const mainWindow = new BrowserWindow({
+    width: 780,
+    height: 460
+  })
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+  mainWindow.setMenu(null)
 })
