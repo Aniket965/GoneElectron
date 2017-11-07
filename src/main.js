05dee7fd28dcda9e64c6f46198c6325f2e78b7fb
aniket965 <aniket965.as@gmail.com>
2017-11-07 20:51:57 +0530

    fixed Notification ❤️

--- src/main.js
+++ src/main.js
@@ -2,17 +2,19 @@ const electron = require('electron')
 const os = require('os')
 const storage = require('electron-json-storage')
 const dataPath = storage.getDataPath()
-const { app, BrowserWindow, ipcMain, Notification } = electron
+const { app, BrowserWindow, ipcMain} = electron
 const SECS_IN_DAY = 10
 storage.setDataPath(os.tmpdir())
-
+var eNotify
 let tasks = new Array()
 
 app.on('ready', _ => {
+
   const mainWindow = new BrowserWindow({
     width: 780,
     height: 460
   })
+  eNotify = require('electron-notify');
   storage.get('tasks', (err, data) => {
     if (!Object.keys(data).length === 0 && data.constructor === Object) {
       tasks.push(data)
@@ -46,13 +48,8 @@ function checkForTaskTime(mainWindow) {
         * Deletes from ui
         */
         // Notify users that task deleted
-        console.log(task)
-        let DeleteNotification = new Notification(`Task time up`, {
-          body: `LOL`
-        })
-        DeleteNotification.show()
-        DeleteNotification.on('click', () => console.log(':P'))
         mainWindow.webContents.send('deletetask', task.date)
+        eNotify.notify({ title: 'Task Timedout', text: `${task.name}` })
         ischanged = true
         return false
       } else {
